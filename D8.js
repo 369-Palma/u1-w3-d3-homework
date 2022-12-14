9c60e11b2d14f8cc9e60f0ab5de118034e39268c
Palma Iacobelli <palmaiacobelli92@gmail.com>
2022-12-14 21:29:13 +0100

    updates

--- D8.js
+++ D8.js
@@ -4,7 +4,7 @@
 
 const changeTitle = function () {
   const NewTitle = document.querySelector("h1");
-  NewTitle.innerText = "Accappatoio";
+  NewTitle.innerText = "Nuovo titolo";
 };
 
 changeTitle();
@@ -17,7 +17,7 @@ const addClassToTitle = function () {
   const NewClass = document.querySelector("h1");
   NewClass.classList.add("myHeading");
   //NewClass.setAttribute("class", "newClass");
-  /* NewClass.classList.add("myHeading"); */
+  NewClass.classList.add("myHeading");
   console.log(NewClass);
 };
 
@@ -38,13 +38,18 @@ changePcontent();
 /* ESERCIZIO 4
        Scrivi una funzione che cambi la proprietà href di ogni link (tranne quello nel footer) con il valore https://www.google.com
       */
-
 /* const changeUrls = function () {
   const ChangeA = document.getElementsByClassName("perlink");
   ChangeA.setAttribute("href", "https://www.google.com"); //Uncaught TypeError: ChangeA.setAttribute is not a function
 };
 changeUrls(); */
-
+const changeUrls = function () {
+  //   aHrefs = document.getElementsByTagName("a");
+  aHrefs = document.querySelectorAll("a:not(footer a)");
+  for (let a = 0; a < aHrefs.length; a++) {
+    aHrefs[a].href = "https://www.google.com";
+  }
+};
 /* ESERCIZIO 5
        Scrivi una funzione che aggiunga un nuovo elemento lista alla seconda lista non ordinata
     */
@@ -61,7 +66,13 @@ addToTheSecond();
        Scrivi una funzione che aggiunga un secondo paragrafo al primo div
     */
 
-const addParagraph = function () {};
+const addParagraph = function () {
+  const nuovaP = document.createElement("p");
+  nuovaP.innerText = "Sono il secondo paragrafo del primo div";
+  const padre = document.getElementsByTagName("div");
+  padre[0].append(nuovaP);
+};
+addParagraph();
 
 /* ESERCIZIO 7
        Scrivi una funzione che faccia scomparire la prima lista non ordinata
@@ -88,20 +99,27 @@ paintItGreen();
        Scrivi una funzione che rimuova l'ultima lettera dall'h1 ogni volta che l'utente lo clicca
       */
 
-const makeItClickable = function () {};
+/*   const makeItClickable = function () {
+  const LettereRimaste = document.getElementsByTagName("h1").onclick =
+    LettereRimaste[0]=LettereRimaste.innerText.slice(0,-1);
+  //const LettereRimasteArray = Array.from(LettereRimaste);
+};
+
+makeItClickable();  */
 
 /* ESERCIZIO 10
        Crea una funzione che, al click sul footer, riveli l'URL del link interno come contenuto di un alert()
       */
 
-const revealFooterLink = function () {};
-
+/* const revealFooterLink = function () {
+  const footerLink = document.querySelector("footer a").href;
+  alert(footerLink);
+};
+ */
 /* ESERCIZIO 11
        Crea una funzione che crei una tabella nell'elemento con id "tableArea". 
        La tabella avrà 5 elementi e questa struttura: immagine, nome prodotto, quantità, prezzo
     */
-
-const generateTable = function () {};
 
 /* ESERCIZIO 12
        Crea una funzione che aggiunga una riga alla tabella precedentemente creata e fornisca i dati necessari come parametri
@@ -124,5 +142,23 @@ const changeColorWithRandom = function () {};
 /* EXTRA ESERCIZIO 16
       Crea una funzione che elimini le vocali da ogni elemento testuale della pagina (puoi aiutarti con i nuovi metodi degli array di ES6)
     */
+/* const deleteVowels = function () {
+  document.querySelectorAll("h1,h2,h3,p,a,li,th,td").forEach(
+    (node) =>
+      (node.innerText = [...node.innerText]
+        .filter((char) => {
+          char = char.toLowerCase();
+          return (
+            char !== "e" &&
+            char !== "a" &&
+            char !== "i" &&
+            char !== "o" &&
+            char !== "u"
+          );
+        })
+        .join(""))
+  );
+};
 
-const deleteVowels = function () {};
+deleteVowels();
+ */
